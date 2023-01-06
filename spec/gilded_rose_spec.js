644cc24b09541fb9d5c2ddcaa8c5495bebe8fee1
Sangyeon Pak <sangyeonpak1994@gmail.com>
2023-01-06 12:05:57 -0500

    Test suite complete

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,20 +1,121 @@
-describe("update_quality", function() {
-  it("degrades an item twice as fast, when sell_in days is less than zero", function () {
-    items.push(new Item("Poisoned Dagger", -5, 20));
+describe("update_quality", () => {
+  it("degrades an item twice as fast, when sell_in days is less than zero", () => {
+    const testItem = new Common("+5 Dexterity Vest", -1, 4);
+    items.push(testItem);
     update_quality();
-    const updatedItem = items[6];
-    expect(updatedItem.quality).toEqual(18);
+    expect(testItem.quality).toEqual(2);
+    expect(testItem.sell_in).toEqual(-2);
+  });
+
+  it("does not reduce the quality of an item that is already zero", () => {
+    const testItem = new Common("+5 Dexterity Vest", 4, 0);
+    items.push(testItem);
+    update_quality();
+    expect(testItem.quality).toEqual(0);
+  });
+
+  it("increases the quality of items named 'Aged Brie' rather than decreasing it", () => {
+    const testItem = new AgedCheese("Aged Brie", 10, 4);
+    items.push(testItem);
+    update_quality();
+    expect(testItem.quality).toEqual(5);
+  });
+
+  it("does not increase the quality of an item with quality = 50", () => {
+    const testItem = new AgedCheese("Aged Brie", 10, 50);
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(50);
+  });
+
+  it("does not decrease the quality of items named 'Sulfuras, Hand of Ragnaros'", () => {
+    const testItem = new Legendary("Sulfuras, Hand of Ragnaros", 0, 80);
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(80);
+    expect(testItem.sell_in).toEqual(0);
+  });
+
+  it("increases quality of backstage passes by 1 when there are 11 days or more", () => {
+    const testItem = new StagePass(
+      "Backstage passes to a TAFKAL80ETC concert",
+      11,
+      3
+    );
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(4);
+  });
+
+  it("increases quality of backstage passes by 2 when there are 10 days or less", () => {
+    const testItem = new StagePass(
+      "Backstage passes to a TAFKAL80ETC concert",
+      9,
+      4
+    );
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(6);
+  });
+
+  it("increases quality of backstage passes by 3 when there are 5 days or less", () => {
+    const testItem = new StagePass(
+      "Backstage passes to a TAFKAL80ETC concert",
+      5,
+      2
+    );
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(5);
+  });
+
+  it("sets quality of backstage passes to 0 when concert has ended", () => {
+    const testItem = new StagePass(
+      "Backstage passes to a TAFKAL80ETC concert",
+      -1,
+      7
+    );
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(0);
+  });
+
+  it("sets quality of backstage passes to 0 when concert has ended", () => {
+    const testItem = new StagePass(
+      "Backstage passes to a TAFKAL80ETC concert",
+      -1,
+      7
+    );
+    items.push(testItem);
+
+    update_quality();
+
+    expect(testItem.quality).toEqual(0);
+  });
+
+  it("decreases by 2 if sell_in is positive", () => {
+    const testItem = new Conjured('Conjured HP Cake', 4, 10);
+    items.push(testItem);
+    update_quality();
+    expect(testItem.quality).toEqual(8);
   })
-  it("does not reduce the quality of an item that is already zero", function () {
-    items.push(new Item("Bramble Vest", 8, 0));
+
+  it("decreases by 4 if sell_in is negative", () => {
+    const testItem = new Conjured('Conjured HP Cake', -1, 10);
+    items.push(testItem);
     update_quality();
-    const updatedItem = items[7];
-    expect(updatedItem.quality).toEqual(0);
-  })
-  it("Aged Brie increases in quality the older it gets", function () {
-    update_quality();
-    console.log(items[1]);
-    const agedBrie = items[1];
-    expect(agedBrie.quality).toEqual(1);
+    expect(testItem.quality).toEqual(6);
   })
 });
--- src/changed_rose.js
+++ src/changed_rose.js
@@ -46,7 +46,6 @@ class StagePass extends Item {
     super(name, sell_in, quality);
   }
   updateQuality () {
-    this.sell_in--;
     if (this.quality < 50) {
       if (this.sell_in < 0)
         this.quality = 0;
@@ -57,6 +56,7 @@ class StagePass extends Item {
       else
         this.quality++;
     }
+    this.sell_in--;
   }
 }
 
@@ -79,26 +79,26 @@ const items = [];
 
 const vest_dex = new Common('+5 Dexterity Vest', 10, 20);
 const elixir_mongoose = new Common('Elixir of the Mongoose', -1, 7)
-const silver_armor = new Common('Silver Cuirass', 3, 0)
+// const silver_armor = new Common('Silver Cuirass', 3, 0)
 const cheese_brie = new AgedCheese('Aged Brie', 2, 0);
 const sulfuras = new Legendary('Sulfuras, Hand of Ragnaros', 0, 80)
 const backstage_TAFKA = new StagePass('Backstage passes to a TAFKAL80ETC concert', 15, 20)
-const backstage_TAFKA2 = new StagePass('7 days left to a TAFKAL80ETC concert', 7, 20)
-const backstage_TAFKA3 = new StagePass('3 days left to a TAFKAL80ETC concert', 3, 20)
-const backstage_TAFKA4 = new StagePass('Expired pass to a TAFKAL80ETC concert', 1, 20)
-const conjured_hpCake = new Conjured('Conjured HP Cake', 4, 10)
+// const backstage_TAFKA2 = new StagePass('7 days left to a TAFKAL80ETC concert', 7, 20)
+// const backstage_TAFKA3 = new StagePass('3 days left to a TAFKAL80ETC concert', 3, 20)
+// const backstage_TAFKA4 = new StagePass('Expired pass to a TAFKAL80ETC concert', 1, 20)
+// const conjured_hpCake = new Conjured('Conjured HP Cake', 4, 10)
 const conjured_manaCake = new Conjured('Conjured Mana Cake', 0, 10)
 
-items.push(vest_dex, elixir_mongoose, silver_armor, cheese_brie, sulfuras, backstage_TAFKA, backstage_TAFKA2, backstage_TAFKA3, backstage_TAFKA4, conjured_manaCake, conjured_hpCake);
+items.push(vest_dex, elixir_mongoose, /* silver_armor,*/ cheese_brie, sulfuras, backstage_TAFKA, /* backstage_TAFKA2, backstage_TAFKA3, backstage_TAFKA4, conjured_hpCake, */ conjured_manaCake );
 
-function passOneDay() {
+function update_quality() {
   for (let item of items){
-    console.log("======================================================================")
-    console.log(item);
+    // console.log("======================================================================")
+    // console.log(item);
     // console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(item)));
     item.updateQuality();
-    console.log(item);
+    // console.log(item);
   }
 }
 
-passOneDay();
+update_quality();
